ef9cce04ec5b9afbb460844b4835b00866341961
张剑 <afzhangjian@163.com>
2017-12-14 09:38:42 +0800

    修改bug

--- js/HomePage/HomePageManager.js
+++ js/HomePage/HomePageManager.js
@@ -39,7 +39,7 @@ function findTitleImageByState() {
 					$('#swiperbox').html(str);
 					var mySwiper = new Swiper('#container1', {
 							loop: true,
-							autoplay: 1000,
+							autoplay: 6000,
 							paginationClickable: true
 						})
 				}
@@ -289,3 +289,51 @@ function getNewestArticle() {
 		}
 	});
 }
+//加入收藏 设为首页
+function AddFavorite(title, url) {
+ try {
+   window.external.addFavorite(url, title);
+ }
+catch (e) {
+   try {
+    window.sidebar.addPanel(title, url, "");
+  }
+   catch (e) {
+     alert("抱歉，您所使用的浏览器无法完成此操作。\n\n加入收藏失败，请进入新网站后使用Ctrl+D进行添加");
+   }
+ }
+}
+
+function SetHome(obj,url){
+  try{
+    obj.style.behavior='url(#default#homepage)';
+    obj.setHomePage(url);
+  }catch(e){
+    if(window.netscape){
+     try{
+       netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+     }catch(e){
+       alert("抱歉，此操作被浏览器拒绝！\n\n请在浏览器地址栏输入“about:config”并回车然后将[signed.applets.codebase_principal_support]设置为'true'");
+     }
+    }else{
+    alert("抱歉，您所使用的浏览器无法完成此操作。\n\n您需要手动将【"+url+"】设置为首页。");
+    }
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/HttpRequest.js
+++ js/HttpRequest.js
@@ -4,110 +4,118 @@
 
 
 document.write("<script language=\"javascript\" src=\"js/jquery.min.js\"><\/script>");
-// var BaseUrl = 'http://192.168.100.188:8080';
-var BaseUrl = 'http://47.104.8.66:8080';
+  var BaseUrl = 'http://192.168.100.190:8080';
+//var BaseUrl = 'http://47.104.8.66:8080';
 
 
-function getByClass(classnames){
-    var classElements = [],allElements = document.getElementsByTagName('*');
-    for (var i=0; i< allElements.length; i++ ) {
-        if (allElements[i].className == classnames) {
-            classElements[classElements.length] = allElements[i];
-        }
-    }
-    return classElements;
+function getByClass(classnames) {
+	var classElements = [],
+		allElements = document.getElementsByTagName('*');
+	for (var i = 0; i < allElements.length; i++) {
+		if (allElements[i].className == classnames) {
+			classElements[classElements.length] = allElements[i];
+		}
+	}
+	return classElements;
 }
 
-function getById(idname){
-    var classElements = [],allElements = document.getElementsByTagName('*');
-    for (var i=0; i< allElements.length; i++ ) {
-        if (allElements[i].id == idname) {
-            classElements[classElements.length] = allElements[i];
-        }
-    }
-    return classElements;
+function getById(idname) {
+	var classElements = [],
+		allElements = document.getElementsByTagName('*');
+	for (var i = 0; i < allElements.length; i++) {
+		if (allElements[i].id == idname) {
+			classElements[classElements.length] = allElements[i];
+		}
+	}
+	return classElements;
 }
 
 var http = {};
 
-http.quest = function (option, callback) {
-    var url = option.url;
-    var method = option.method;
-    var data = option.data;
-    var timeout = option.timeout || 1000*30;
+http.quest = function(option, callback) {
+	var url = option.url;
+	var method = option.method;
+	var data = option.data;
+	var timeout = option.timeout || 1000 * 30;
 
-    var xhr = new XMLHttpRequest();
-    (timeout > 0) && (xhr.timeout = timeout);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status >= 200 && xhr.status < 400) {
-                var result = xhr.responseText;
-                try {result = JSON.parse(xhr.responseText);} catch (e) {}
-                callback && callback(null, result);
-            } else {
-                callback && callback('status: ' + xhr.status);
-            }
-        }
-    }.bind(this);
-    xhr.open(method, url, true);
-    xhr.setRequestHeader("Content-Type","application/json");
-    if(typeof data === 'object'){
-        try{
-            data = JSON.stringify(data);
-        }catch(e){}
-    }
-    xhr.send(data);
-    xhr.ontimeout = function () {
-        callback && callback('timeout');
-        console.log('%c连%c接%c超%c时', 'color:red', 'color:orange', 'color:purple', 'color:green');
-    };
+	var xhr = new XMLHttpRequest();
+	(timeout > 0) && (xhr.timeout = timeout);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+			if (xhr.status >= 200 && xhr.status < 400) {
+				var result = xhr.responseText;
+				try {
+					result = JSON.parse(xhr.responseText);
+				} catch (e) {}
+				callback && callback(null, result);
+			} else {
+				callback && callback('status: ' + xhr.status);
+			}
+		}
+	}.bind(this);
+	xhr.open(method, url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	if (typeof data === 'object') {
+		try {
+			data = JSON.stringify(data);
+		} catch (e) {}
+	}
+	xhr.send(data);
+	xhr.ontimeout = function() {
+		callback && callback('timeout');
+		console.log('%c连%c接%c超%c时', 'color:red', 'color:orange', 'color:purple', 'color:green');
+	};
 
 };
 
-http.get = function (url, callback) {
-    var option = url.url ? url : { url: url };
-    option.method = 'get';
-    option.timeout = 1000*30;
-    this.quest(option, callback);
+http.get = function(url, callback) {
+	var option = url.url ? url : {
+		url: url
+	};
+	option.method = 'get';
+	option.timeout = 1000 * 30;
+	this.quest(option, callback);
 };
 
-http.post = function (option, callback) {
-    option.method = 'post';
-    option.timeout = 1000*30;
-    this.quest(option, callback);
+http.post = function(option, callback) {
+	option.method = 'post';
+	option.timeout = 1000 * 30;
+	this.quest(option, callback);
 };
 
 
 
 //友情链接   合作媒体
 
-function Link(type,callBack){
+function Link(type, callBack) {
 	//type=3改2
 	if (type == 3) type = 2;
-    http.get(BaseUrl+"/frontEnd/getBlogroll?type="+type,function(err,result){
-        if(result.status == '0'){
-            callBack(result)
-        }
-    })
+	http.get(BaseUrl + "/frontEnd/getBlogroll?type=" + type, function(err, result) {
+		if (result.status == '0') {
+			callBack(result)
+		}
+	})
 }
 
-$('.item').click(function(){
-    $(this).addClass('active').siblings().removeClass('active');
-    var index = $(this).index()+1;
-    Link(index,function(result){
-        var html = '';
-        var data = result.result;
-        for(var i=0;i<data.length;i++){
-            html += '<li><a href="'+data[i].linkurl+'"><img src="'+data[i].imgUrl+'" alt=""></a></li>'
-        }
-        $('.linkContent ul').html(html)
-    })
+$('.item').click(function() {
+	$(this).addClass('active').siblings().removeClass('active');
+	var index = $(this).index() + 1;
+	Link(index, function(result) {
+		var html = '';
+		var data = result.result;
+		for (var i = 0; i < data.length; i++) {
+			html += '<li><a href="' + data[i].linkurl + '"><img src="' + data[i].imgUrl + '" alt=""></a></li>'
+		}
+		$('.linkContent ul').html(html)
+	})
 })
-Link(1,function(result){
-    var html = '';
-    var data = result.result;
-    for(var i=0;i<data.length;i++){
-        html += '<li><a href="'+data[i].linkurl+'"><img src="'+data[i].imgUrl+'" alt=""></a></li>'
-    }
-    $('.linkContent ul').html(html)
+Link(1, function(result) {
+	var html = '';
+	var data = result.result;
+	for (var i = 0; i < data.length; i++) {
+		html += '<li><a href="' + data[i].linkurl + '"><img src="' + data[i].imgUrl + '" alt=""></a></li>'
+	}
+	$('.linkContent ul').html(html)
 })
+
+
